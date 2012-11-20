3ef1fe5309591aca3d5b74a826c43396dd1531aa
Rafal Sniezynski <rafal@sniezynski.pl>
2012-11-20 16:19:54 +0100

    Added support for named contexts

--- django_sorting/middleware.py
+++ django_sorting/middleware.py
@@ -1,13 +1,19 @@
-def get_field(self):
+def get_field(self, name=None):
     try:
-        field = self.REQUEST['sort']
+        if name:
+            field = self.REQUEST["sort_" + name]
+        else:
+            field = self.REQUEST['sort']
     except (KeyError, ValueError, TypeError):
         field = ''
-    return (self.direction == 'desc' and '-' or '') + field
+    return (self.get_direction(name) == 'desc' and '-' or '') + field
 
-def get_direction(self):
+def get_direction(self, name=None):
     try:
-        return self.REQUEST['dir']
+        if name:
+            return self.REQUEST['dir_' + name]
+        else:
+            return self.REQUEST['dir']
     except (KeyError, ValueError, TypeError):
         return 'desc'
 
@@ -18,5 +24,5 @@ class SortingMiddleware(object):
     portions of the request.
     """
     def process_request(self, request):
-        request.__class__.field = property(get_field)
-        request.__class__.direction = property(get_direction)
+        request.__class__.get_field = get_field
+        request.__class__.get_direction = get_direction
--- django_sorting/templatetags/sorting_tags.py
+++ django_sorting/templatetags/sorting_tags.py
@@ -21,12 +21,13 @@ def anchor(parser, token):
     """
     bits = [b.strip('"\'') for b in token.split_contents()]
     if len(bits) < 2:
-        raise TemplateSyntaxError, "anchor tag takes at least 1 argument"
+        raise template.TemplateSyntaxError, "anchor tag takes at least 1 argument"
     try:
         title = bits[2]
     except IndexError:
         title = bits[1].capitalize()
-    return SortAnchorNode(bits[1].strip(), title.strip())
+    name = len(bits) == 4 and bits[3].strip() or None
+    return SortAnchorNode(bits[1].strip(), title.strip(), name)
     
 
 class SortAnchorNode(template.Node):
@@ -41,26 +42,45 @@ class SortAnchorNode(template.Node):
         <a href="/the/current/path/?sort=name" title="Name">Name</a>
 
     """
-    def __init__(self, field, title):
+    def __init__(self, field, title, name=None):
         self.field = field
         self.title = title
+        self.name = name
 
     def render(self, context):
         request = context['request']
         getvars = request.GET.copy()
-        if 'sort' in getvars:
-            sortby = getvars['sort']
-            del getvars['sort']
+        if self.name:
+            field_var = "sort_" + self.name
+            dir_var = "dir_" + self.name
+        else:
+            field_var = "sort"
+            dir_var = "dir"
+        sort_default = field_var + "_default"
+        if field_var in getvars:
+            sortby = getvars[field_var]
+            del getvars[field_var]
         else:
             sortby = ''
-        if 'dir' in getvars:
-            sortdir = getvars['dir']
-            del getvars['dir']
+        if dir_var in getvars:
+            sortdir = getvars[dir_var]
+            del getvars[dir_var]
         else:
             sortdir = ''
         if sortby == self.field:
-            getvars['dir'] = sort_directions[sortdir]['inverse']
+            getvars[dir_var] = sort_directions[sortdir]['inverse']
             icon = sort_directions[sortdir]['icon']
+        elif not sortby and sort_default in context:
+            sort_default_order = context[sort_default]
+            sort_default_field = sort_default_order.lstrip("-")
+            if self.field == sort_default_field:
+                sortdir = "desc" if sort_default_order.startswith("-") else "asc"
+                icon = sort_directions[sortdir]['icon']
+                getvars[dir_var] = sort_directions[sortdir]['inverse']
+            else:
+                icon = ""
+
+
         else:
             icon = ''
         if len(getvars.keys()) > 0:
@@ -72,28 +92,29 @@ class SortAnchorNode(template.Node):
         else:
             title = self.title
 
-        url = '%s?sort=%s%s' % (request.path, self.field, urlappend)
+
+        url = '%s?%s=%s%s' % (request.path, field_var, self.field, urlappend)
         return '<a href="%s" title="%s">%s</a>' % (url, self.title, title)
 
 
 def autosort(parser, token):
     bits = [b.strip('"\'') for b in token.split_contents()]
-    if len(bits) != 2:
-        raise TemplateSyntaxError, "autosort tag takes exactly one argument"
-    return SortedDataNode(bits[1])
+    if len(bits) > 3:
+        raise template.TemplateSyntaxError, "autosort tag takes exactly at most two arguments"
+    return SortedDataNode(bits[1], bits[2] if len(bits) > 2 else None)
 
 class SortedDataNode(template.Node):
     """
     Automatically sort a queryset with {% autosort queryset %}
     """
-    def __init__(self, queryset_var, context_var=None):
+    def __init__(self, queryset_var, name=None):
         self.queryset_var = template.Variable(queryset_var)
-        self.context_var = context_var
+        self.name = name
 
     def render(self, context):
         key = self.queryset_var.var
         value = self.queryset_var.resolve(context)
-        order_by = context['request'].field
+        order_by = context['request'].get_field(self.name)
         if len(order_by) > 1:
             try:
                 context[key] = value.order_by(order_by)
